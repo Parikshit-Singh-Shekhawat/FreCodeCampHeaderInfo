7b11d17b9e3829c64d02938a16d7a040eed88595
Parikshit <shekhawat.p.singh@gmail.com>
2021-08-31 10:09:50 +0530

    whoami changes

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.connection.remoteAddress,
+  language: req.headers["accept-language"],
+  software: req.headers["user-agent"]});
+});
 
 
 // listen for requests :)
